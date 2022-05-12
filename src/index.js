3a5e394fb97f9ec80ea4661fa14ed2d7b0b1eaef
Mak513 <osei.makangu@hotmail.com>
2022-05-12 15:16:08 +0200

    frontend react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1.) BrowserRouter, Routes und Route
+//importieren
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import PlayerList from "./components/PlayerList";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*2.) App in BrowserRouter schachteln */}
+    <BrowserRouter>
+        {/*3.) App in Routes schachteln */}
+      <Routes>
+        <Route path="/" element={<App />}/>
+                <Route path="/nationalteam" element={<PlayerList/>}/>
+      <Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
